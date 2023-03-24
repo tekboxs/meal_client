93700530e7a03ea12b41eb05338b337372df84c7
Tekboxs <tekboxs@gmail.com>
2023-03-24 17:05:27 -0400

    export fix

--- lib/meal_client.dart
+++ lib/meal_client.dart
@@ -6,3 +6,4 @@ export 'src/data/meal/meal_api_client.dart';
 export 'src/data/meal/meal_uno_initializer.dart';
 export 'src/data/meal/meal_uno_interceptors.dart';
 export 'src/domain/meal/client_keys.dart';
+export 'src/domain/meal/i_meal_client.dart';
